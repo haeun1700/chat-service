32ab086e91fd2e77a28ffd62cea296cd99d33be1
haeun1700 <haeun7173@gmail.com>
2025-03-11 05:59:04 +0900

    Initial commit

---
+++ src/test/java/com/example/charservice/CharServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.charservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
